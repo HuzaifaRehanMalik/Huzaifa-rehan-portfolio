3aea332ebf177fe28ce7e0dbf811003dade8d3f4
HUZAIFA REHAN <rehanhuzaifa035@gmail.com>
2024-11-10 22:15:18 +0500

    small up date

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ export default function about() {
         <Image src="\my pic.jpg" alt="" width="400" height="50" className="py-20 px-20  " />
           <div>
             <h1 className="text-center text-[50px] font-extrabold">Introduction</h1>
-            <div className="text-xl font-bold text-center leading-tight">I am a Computer Science student at the University of the Karachi, pursuing ADS (Associate Degree in Science) in Computer Science. My passion lies in exploring the realms of Generative AI and large language models, combining my interest in AI with hands-on skills in Python, JavaScript, TypeScript, HTML, and CSS. Currently, I am diving into Next.js to expand my web development toolkit and further prepare for a future of creating innovative, tech-driven solutions.</div>
+            <div className="text-xl font-bold text-center leading-tight">I'm a Computer Science student at the University of the Karachi, pursuing ADS (Associate Degree in Science) in Computer Science. My passion lies in exploring the realms of Generative AI and large language models, combining my interest in AI with hands-on skills in Python, JavaScript, TypeScript, HTML, and CSS. Currently, I'm diving into Next.js to expand my web development toolkit and further prepare for a future of creating innovative, tech-driven solutions.</div>
             <div className="text-center text-[40px] font-extrabold  py-5 px-5">
               <Link href="/cv.pdf">
                 <button className=" bg-blue-400 rounded-full w-96 h-20">DOWNLOAD CV</button>
