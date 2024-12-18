b7bb5d4b5021e1a4c7302dc19bbdcd408428a8c8
HUZAIFA REHAN <rehanhuzaifa035@gmail.com>
2024-12-18 23:18:06 +0500

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,11 +6,11 @@ export default function Hero() {
           <div className="py-20 px-20">
 
             <h1 className="text-[80px] font-extrabold leading-tight">
-                I'm 
+                I&apos;m 
                 Huzaifa 
                 Rehan
             </h1>
-            <p>I'm passionate about exploring Generative AI and large language models, combining my interest in AI with hands-on skills in Python, JavaScript, TypeScript, HTML, and CSS. Currently, I'm diving into Next.js to expand my web development toolkit and further prepare for a future of creating innovative, tech-driven solutions.</p>
+            <p>I&apos;m passionate about exploring Generative AI and large language models  combining my interest in AI with hands-on skills in Python, JavaScript, TypeScript, HTML, and CSS. Currently, I&apos;m diving into Next.js to expand my web development toolkit and further prepare for a future of creating innovative, tech-driven solutions.</p>
           </div>
 
             <Image src="/mypic.jpg" alt="My pic" width="300" height="50" className="py-10 m-10 " />
