d0c3663ad5a01aa7d038627126f71f84568dc4a5
HUZAIFA REHAN <rehanhuzaifa035@gmail.com>
2024-12-18 22:40:02 +0500

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
             <p>I'm passionate about exploring Generative AI and large language models, combining my interest in AI with hands-on skills in Python, JavaScript, TypeScript, HTML, and CSS. Currently, I'm diving into Next.js to expand my web development toolkit and further prepare for a future of creating innovative, tech-driven solutions.</p>
           </div>
 
-            <Image src="/mypic.jpg" alt="My pic" width="400" height="50"  />
+            <Image src="/mypic.jpg" alt="My pic" width="300" height="50" className="py-10 m-10 " />
         
         </div>
     </div>
